b8fd902a8da70fe7c64a5982ab05fbc48832b522
Re-taro <aminokacho@gmail.com>
2022-02-19 20:43:57 +0900

    ✨ add the html buffer

---
+++ source/index.ts
@@ -0,0 +1,25 @@
+import he from 'he'
+import * as sanitizeXMLCharacters from 'sanitize-xml-string'
+
+export default html
+export type HTML = string
+export const html = (template: TemplateStringsArray, ...substitutions: (string | string[])[]): HTML => {
+  const templatePartsSanitized = template.map(templatePart => sanitizeXMLCharacters.sanitize(templatePart))
+  const substitutionsSanitized = [
+    ...substitutions.map(substitution =>
+      (Array.isArray(substitution) ? substitution : [substitution]).map(substitutionPart =>
+        sanitizeXMLCharacters.sanitize(String(substitutionPart))
+      )
+    ),
+    []
+  ]
+  const buffer = new Array<string>()
+  for (const index of templatePartsSanitized.keys()) {
+    let templatePart = templatePartsSanitized[index]
+    let substitution = substitutionsSanitized[index]
+    if (templatePart.endsWith('$')) templatePart = templatePart.slice(0, -1)
+    else substitution = substitution.map(substitutionPart => he.encode(substitutionPart))
+    buffer.push(templatePart, ...substitution)
+  }
+  return buffer.join('')
+}
